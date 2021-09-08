cdbc03cc41e34c6fdfbe3315e40bbef0db409fa8
bonnlee <15238603721@163.com>
2021-09-08 09:29:44 +0800

    first commit

---
+++ src/main/java/com/bonnlee/Student.java
@@ -0,0 +1,18 @@
+package com.bonnlee;
+
+public class Student {
+    private String name;
+
+    public Student() {
+        System.out.println("A student！");
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
